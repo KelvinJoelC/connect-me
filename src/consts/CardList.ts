94009ce23267f17c702ee3288f14f1d57e967923
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-07-05 14:38:47 +1000

    UX

---
+++ src/consts/CardList.ts
@@ -0,0 +1,20 @@
+export const CardList = [
+{
+    title: 'GitHub',
+    url: 'https://github.com/kelvin',
+    description: 'Mis proyectos y código',
+    icon: 'icons/github.svg'
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://linkedin.com/in/kelvin',
+    description: 'Mi perfil profesional',
+    icon: 'icons/linkedin.svg'
+  },
+  {
+    title: 'Instagram',
+    url: 'https://instagram.com/kelvin',
+    description: 'Mi lado personal',
+    icon: 'icons/instagram.svg'
+  }
+]
